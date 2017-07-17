f2f20edb0df47bbf9f21e1b7d5997043ce69b4a7
Ben Blackburn <benjamin.blackburn@gmail.com>
2017-07-17 08:31:59 -0500

    add 'move' key to li in Game render()

--- src/index.js
+++ src/index.js
@@ -75,6 +75,15 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Move #' + move : 'Game Start';
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      )
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -92,7 +101,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
